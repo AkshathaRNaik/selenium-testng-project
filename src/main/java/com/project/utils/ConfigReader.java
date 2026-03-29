c1669e6f4facf3e54791b3bdf32204a40d10c06f
Akshatha Naik <akshathanaik543@gmail.com>
2026-03-29 16:30:49 +0530

    First commit : implemented config properties file

---
+++ src/main/java/com/project/utils/ConfigReader.java
@@ -0,0 +1,31 @@
+package com.project.utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+private static Properties properties;
+    static {
+        try {
+            System.out.println(System.getProperty("user.dir"));
+            FileInputStream fileInputStream = new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/config/config.properties");
+            System.out.println("after fileinputstream");
+            properties = new Properties();
+            properties.load(fileInputStream);
+        } catch (FileNotFoundException e) {
+            System.out.println("file not found Exception");
+        }
+        catch(IOException eo)
+        {
+            System.out.println("IO Exception");
+        }
+    }
+    public static String getUsername(){
+       return properties.getProperty("Username");
+    }
+    public static String getPassword() {
+        return properties.getProperty("Password");
+    }
+}
